b3a37e653f6056e95c57842c5e4333e1fbbc94f4
Daniel Graham <dpgraham.85@gmail.com>
2017-02-07 11:04:06 -0800

    Stop trying to start server after 10 failed attempts

--- test/unit/iwdp-specs.js
+++ test/unit/iwdp-specs.js
@@ -7,6 +7,7 @@ import logger from '../../lib/device-log/logger';*/
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import IWDP from '../../lib/iwdp';
+import { SubProcess } from 'teen_process';
 // import B from 'bluebird';
 
 
@@ -29,5 +30,15 @@ describe.only('ios webkit debug proxy class', () => {
     await iwdpInstance.start();
     let devices = await iwdpInstance.getDevices();
     expect(devices.length).to.equal(0);
+  });
+
+  it('should stop trying to start IWDP server after failed attempts', async function (done) {
+    // Change process to something that won't work
+    iwdpInstance.process = new SubProcess('non-existent-command');
+    iwdpInstance.on('failure', () => {
+      expect(iwdpInstance.getDevices()).to.be.rejected;
+      done();
+    });
+    iwdpInstance.start();
   });
 });
